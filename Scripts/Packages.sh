9508e6bf7a7e8242ca13ad7b2e6ecb2f91874495
EDE <Insshells@gmail.com>
2025-11-01 06:26:23 +0800

    remove luci-app-adguardhome

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -73,8 +73,6 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
-UPDATE_PACKAGE "luci-app-adguardhome" "stevenjoezhang/luci-app-adguardhome" "dev"
-
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
